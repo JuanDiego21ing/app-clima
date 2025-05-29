014f5873f30f11ad7e7fb781c2afe9a8b33eadec
joantellees <fjoan9169@gmail.com>
2025-05-29 15:58:42 -0700

    cambios diseno carta caracts

--- componentes/TarjetaClimaCaract.js
+++ componentes/TarjetaClimaCaract.js
@@ -4,29 +4,31 @@ import { View, Text, StyleSheet } from "react-native";
 export default function TarjetaClimaCaract({ datos }) {
   return (
     <View style={estilos.tarjeta}>
-      <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold', color: '#black', marginBottom: 8 }}>Condiciones  Actuales</Text>
-      <Text style={estilos.texto}>
-        🌡️ Sensación térmica: {datos.sensacionTermica.toFixed(1)}°C
-      </Text>
-      <Text style={estilos.texto}>💧 Humedad: {datos.humedad}%</Text>
-      <Text style={estilos.texto}>
-        🌫️ Visibilidad: {datos.visibilidad / 1000} km
-      </Text>
-      <Text style={estilos.texto}>
-        🌬️ Presión atmosférica: {datos.presion} hPa
-      </Text>
-      <Text style={estilos.texto}>
-        💨 Velocidad del viento: {datos.viento} km/h
-      </Text>
-      <Text style={estilos.texto}>☀️ Índice UV: {datos.indiceUV}</Text>
+      <Text style={estilos.titulo}>Condiciones Actuales</Text>
+
+      <View style={estilos.lista}>
+        <Text style={estilos.texto}>
+          🌡️ Sensación térmica: {datos.sensacionTermica.toFixed(1)}°C
+        </Text>
+        <Text style={estilos.texto}>💧 Humedad: {datos.humedad}%</Text>
+        <Text style={estilos.texto}>
+          🌫️ Visibilidad: {datos.visibilidad / 1000} km
+        </Text>
+        <Text style={estilos.texto}>
+          🌬️ Presión atmosférica: {datos.presion} hPa
+        </Text>
+        <Text style={estilos.texto}>
+          💨 Velocidad del viento: {datos.viento} km/h
+        </Text>
+        <Text style={estilos.texto}>☀️ Índice UV: {datos.indiceUV}</Text>
+      </View>
     </View>
   );
 }
 
 const estilos = StyleSheet.create({
-
   tarjeta: {
-    backgroundColor: "#white",
+    backgroundColor: "#ffffff",
     borderRadius: 12,
     padding: 16,
     marginBottom: 15,
@@ -36,13 +38,17 @@ const estilos = StyleSheet.create({
     shadowRadius: 3,
     elevation: 4,
     width: "100%",
-    alignItems: "left",
+    alignItems: "center",
   },
   titulo: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#2c3e50",
-    marginBottom: 8,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  lista: {
+    alignSelf: "flex-start",
   },
   texto: {
     fontSize: 16,
